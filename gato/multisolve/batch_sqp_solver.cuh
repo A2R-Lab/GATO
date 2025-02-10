9fa60003e73e20a6a5e29297ca273e0ba16530a7
alexanderdu15 <alexander.du@columbia.edu>
2025-02-09 23:18:32 -0500

    Added lambda reset, removed return values from kernel interface functions

--- gato/multisolve/batch_sqp_solver.cuh
+++ gato/multisolve/batch_sqp_solver.cuh
@@ -33,9 +33,8 @@ public:
         freeMemory();
     }
 
-    void reset() {
-        // TODO: reset rhos (what else?)
-        return;
+    void resetLambda() {
+        gpuErrchk(cudaMemset(d_lambda_batch_, 0, VEC_SIZE_PADDED * BatchSize * sizeof(T)));
     }
 
     void warmStart() {
@@ -48,8 +47,12 @@ public:
         ProblemInputs<T, BatchSize> inputs
     ) {
         SQPStats<T, BatchSize> sqp_stats;
-        PCGStats pcg_stats;
-        LineSearchStats<T> line_search_stats;
+        PCGStats<BatchSize> pcg_stats;
+        LineSearchStats<T, BatchSize> line_search_stats;
+
+        cudaEvent_t pcg_start, pcg_stop;
+        gpuErrchk(cudaEventCreate(&pcg_start));
+        gpuErrchk(cudaEventCreate(&pcg_stop));
 
         auto solve_start_time = std::chrono::high_resolution_clock::now();
 
@@ -58,12 +61,13 @@ public:
 
         // Reset other arrays
         gpuErrchk(cudaMemset(d_dz_batch_, 0, TRAJ_SIZE * BatchSize * sizeof(T)));
-        gpuErrchk(cudaMemset(d_rho_max_reached_batch_, 0, BatchSize * sizeof(int32_t)));
         gpuErrchk(cudaMemset(d_merit_initial_batch_, 0, BatchSize * sizeof(T)));
         gpuErrchk(cudaMemset(d_merit_batch_, 0, NUM_ALPHAS * BatchSize * sizeof(T)));
+        gpuErrchk(cudaMemset(d_step_size_batch_, 0, BatchSize * sizeof(T)));
+        gpuErrchk(cudaMemset(d_all_rho_max_reached_, 0, sizeof(int32_t)));
+        gpuErrchk(cudaMemset(d_rho_max_reached_batch_, 0, BatchSize * sizeof(int32_t)));
         gpuErrchk(cudaMemset(d_iterations_batch_, 0, BatchSize * sizeof(uint32_t)));
 
-        // Initial Merit
         computeMeritBatched<T, BatchSize, 1>(
             d_merit_initial_batch_,
             d_dz_batch_,
@@ -73,24 +77,39 @@ public:
 
         // SQP Loop
         for (uint32_t i = 0; i < SQP_MAX_ITER; i++) {
+            std::cout << "\nIteration " << i << ":\n";
+            
             setupKKTSystemBatched<T, BatchSize>(
                 kkt_system_batch_,
                 inputs,
                 d_xu_traj_batch
             );
-
+            
             formSchurSystemBatched<T, BatchSize>(
                 schur_system_batch_,
                 kkt_system_batch_,
                 d_rho_penalty_batch_
             );
-
-            pcg_stats = solvePCGBatched<T, BatchSize>(
+            
+            gpuErrchk(cudaEventRecord(pcg_start));
+            solvePCGBatched<T, BatchSize>(
                 d_lambda_batch_,
                 schur_system_batch_,
                 PCG_TOLERANCE,
-                d_rho_max_reached_batch_
+                d_rho_max_reached_batch_,
+                d_pcg_converged_,
+                d_pcg_iterations_
             );
+            gpuErrchk(cudaEventRecord(pcg_stop));
+            gpuErrchk(cudaEventSynchronize(pcg_stop));
+            
+            float pcg_time_ms;
+            gpuErrchk(cudaEventElapsedTime(&pcg_time_ms, pcg_start, pcg_stop));
+            std::cout << "PCG solve time: " << pcg_time_ms * 1000 << " us\n";
+        
+            gpuErrchk(cudaMemcpy(pcg_stats.converged.data(), d_pcg_converged_, sizeof(int32_t) * BatchSize, cudaMemcpyDeviceToHost));
+            gpuErrchk(cudaMemcpy(pcg_stats.num_iterations.data(), d_pcg_iterations_, sizeof(uint32_t) * BatchSize, cudaMemcpyDeviceToHost));
+            pcg_stats.solve_time_us = pcg_time_ms * 1000;
 
             computeDzBatched<T, BatchSize>(
                 d_dz_batch_,
@@ -105,18 +124,23 @@ public:
                 inputs
             );
 
-            line_search_stats = lineSearchAndUpdateBatched<T, BatchSize, NUM_ALPHAS>(
+            lineSearchAndUpdateBatched<T, BatchSize, NUM_ALPHAS>(
                 d_xu_traj_batch,
                 d_dz_batch_,
                 d_merit_batch_,
                 d_merit_initial_batch_,
                 d_rho_penalty_batch_,
                 d_drho_batch_,
+                d_step_size_batch_,
+                d_all_rho_max_reached_,
                 d_rho_max_reached_batch_,
                 d_iterations_batch_
             );
 
             sqp_stats.pcg_stats.push_back(pcg_stats);
+            gpuErrchk(cudaMemcpy(line_search_stats.min_merit.data(), d_merit_initial_batch_, BatchSize * sizeof(T), cudaMemcpyDeviceToHost));
+            gpuErrchk(cudaMemcpy(line_search_stats.step_size.data(), d_step_size_batch_, BatchSize * sizeof(T), cudaMemcpyDeviceToHost));
+            gpuErrchk(cudaMemcpy(&line_search_stats.all_rho_max_reached, d_all_rho_max_reached_, sizeof(int32_t), cudaMemcpyDeviceToHost));
             sqp_stats.line_search_stats.push_back(line_search_stats);
             if (line_search_stats.all_rho_max_reached) break;
         }
@@ -128,6 +152,9 @@ public:
         sqp_stats.solve_time_us = std::chrono::duration_cast<std::chrono::microseconds>(solve_end_time - solve_start_time).count();
         gpuErrchk(cudaMemcpy(sqp_stats.sqp_iterations.data(), d_iterations_batch_, BatchSize * sizeof(uint32_t), cudaMemcpyDeviceToHost));
         gpuErrchk(cudaMemcpy(sqp_stats.rho_max_reached.data(), d_rho_max_reached_batch_, BatchSize * sizeof(int32_t), cudaMemcpyDeviceToHost));
+
+        gpuErrchk(cudaEventDestroy(pcg_start));
+        gpuErrchk(cudaEventDestroy(pcg_stop));
 
         return sqp_stats;
     }
@@ -157,9 +184,13 @@ private:
         gpuErrchk(cudaMalloc(&d_dz_batch_, TRAJ_SIZE * BatchSize * sizeof(T)));
         gpuErrchk(cudaMalloc(&d_rho_penalty_batch_, BatchSize * sizeof(T)));
         gpuErrchk(cudaMalloc(&d_drho_batch_, BatchSize * sizeof(T)));
-        gpuErrchk(cudaMalloc(&d_rho_max_reached_batch_, BatchSize * sizeof(int32_t)));
+        gpuErrchk(cudaMalloc(&d_pcg_converged_, sizeof(int32_t) * BatchSize));
+        gpuErrchk(cudaMalloc(&d_pcg_iterations_, sizeof(uint32_t) * BatchSize));
         gpuErrchk(cudaMalloc(&d_merit_initial_batch_, BatchSize * sizeof(T)));
         gpuErrchk(cudaMalloc(&d_merit_batch_, NUM_ALPHAS * BatchSize * sizeof(T)));
+        gpuErrchk(cudaMalloc(&d_step_size_batch_, BatchSize * sizeof(T)));
+        gpuErrchk(cudaMalloc(&d_all_rho_max_reached_, sizeof(int32_t)));
+        gpuErrchk(cudaMalloc(&d_rho_max_reached_batch_, BatchSize * sizeof(int32_t)));
         gpuErrchk(cudaMalloc(&d_iterations_batch_, BatchSize * sizeof(uint32_t)));
     }
 
@@ -197,8 +228,15 @@ private:
     T *d_rho_penalty_batch_;
     T h_drho_batch_init_[BatchSize];
     T *d_drho_batch_;
-    int32_t *d_rho_max_reached_batch_;
+    // PCG
+    int32_t *d_pcg_converged_;
+    uint32_t *d_pcg_iterations_;
+    // Merit
     T *d_merit_initial_batch_;
     T *d_merit_batch_;
+    // Line search
+    T *d_step_size_batch_;
+    int32_t *d_all_rho_max_reached_;
+    int32_t *d_rho_max_reached_batch_;
     uint32_t *d_iterations_batch_;
 };
