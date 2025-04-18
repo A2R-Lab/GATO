9a570145d235006ad0a62912a3d4e8d6e7d10c96
Alex Du <alexander.du@columbia.edu>
2025-04-17 21:11:32 -0400

    ready for experiments

--- bindings/setup.py
+++ bindings/setup.py
@@ -17,7 +17,6 @@ setup(
         include_dirs=[
             '.',
             '..',
-            './gato',
             '../gato',
             '../config',
             '../dependencies',
--- config/settings.h
+++ config/settings.h
@@ -14,12 +14,12 @@ using T = float;
 constexpr uint32_t KNOT_POINTS = 32;
 //constexpr T TIMESTEP = 0.01; // 1/64 s
 
-constexpr uint32_t SQP_MAX_ITER = 5;
-constexpr uint32_t PCG_MAX_ITER = 200;
+constexpr uint32_t SQP_MAX_ITER = 3;
+constexpr uint32_t PCG_MAX_ITER = 160;
 
 constexpr T PCG_TOLERANCE = static_cast<T>(1e-4); // relative tolerance
 
-constexpr uint32_t NUM_ALPHAS = 8;
+constexpr uint32_t NUM_ALPHAS = 16;
 
 constexpr uint32_t F_EXT_KNOTS = 32;
 
@@ -27,10 +27,10 @@ constexpr uint32_t F_EXT_KNOTS = 32;
 // TODO: SQP max time (const frequency)
 
 // ----- Cost -----
-constexpr float CONTROL_COST = 1e-5;
-constexpr float VELOCITY_COST = 1e-2;
+constexpr float CONTROL_COST = 1e-6;
+constexpr float VELOCITY_COST = 5e-3;
 constexpr float TERMINAL_COST = 50.0;
-constexpr float BARRIER_COST = 0.01;
+constexpr float BARRIER_COST = 0.005;
 
 constexpr float RHO_INIT = 1e-4;
 constexpr float RHO_FACTOR = 1.2;
